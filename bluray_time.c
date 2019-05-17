5c8d9b9e3c5ff1b2eef11c2cd4f0a9bc94f98a6e
Steve Dibb <steve.dibb@gmail.com>
2019-05-17 09:04:02 -0600

    Add functions to get times of chapters

--- bluray_time.c
+++ bluray_time.c
@@ -28,3 +28,44 @@ void bluray_duration_length(char *str, const uint64_t duration) {
 	snprintf(str, BLURAY_DURATION + 1, "%02lu:%02lu:%02lu.%02lu", d_hours, d_mins, d_secs, d_msecs);
 
 }
+
+uint64_t bluray_chapter_duration(struct bluray *bd, const uint32_t title_ix, const uint32_t chapter_ix) {
+
+	int32_t retval = 0;
+	retval = bd_select_title(bd, title_ix);
+
+	if(retval == 0)
+		return 0;
+
+	struct bd_title_info *bluray_title_info = NULL;
+	bluray_title_info = bd_get_title_info(bd, title_ix, 0);
+
+	if(bluray_title_info == NULL)
+		return 0;
+
+	uint32_t chapter_number;
+	chapter_number = chapter_ix + 1;
+
+	if(chapter_number > bluray_title_info->chapter_count)
+		return 0;
+
+	struct bd_chapter *bd_chapter = NULL;
+	bd_chapter = &bluray_title_info->chapters[chapter_ix];
+
+	return bd_chapter->duration;
+
+}
+
+void bluray_chapter_length(char *dest_str, struct bluray *bd, const uint32_t title_ix, const uint32_t chapter_ix) {
+
+	uint64_t duration = 0;
+
+	duration = bluray_chapter_duration(bd, title_ix, chapter_ix);
+
+	char duration_str[BLURAY_DURATION + 1];
+
+	bluray_duration_length(duration_str, duration);
+
+	sprintf(dest_str, "%s", duration_str);
+
+}
--- bluray_time.h
+++ bluray_time.h
@@ -6,11 +6,17 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <inttypes.h>
+#include "libbluray/bluray.h"
 
 uint64_t bluray_duration_seconds(const uint64_t duration);
 
 uint64_t bluray_duration_minutes(const uint64_t duration);
 
 void bluray_duration_length(char *str, const uint64_t duration);
+
+uint64_t bluray_chapter_duration(struct bluray *bd, const uint32_t title_ix, const uint32_t chapter_ix);
+
+void bluray_chapter_length(char *dest_str, struct bluray *bd, const uint32_t title_ix, const uint32_t chapter_ix);
 
 #endif
