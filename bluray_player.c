5c51b8ae66c7f48396954fc3a5f1f7d4012a1a0a
Steve Dibb <steve.dibb@gmail.com>
2019-05-29 14:35:13 -0600

    Display angles in title header

--- bluray_player.c
+++ bluray_player.c
@@ -351,7 +351,8 @@ int main(int argc, char **argv) {
 	if(strlen(bluray_info.disc_name))
 		printf("Disc title: %s\n", bluray_info.disc_name);
 
-	printf("Title: %03" PRIu32 ", Playlist: %04" PRIu32 ", Length: %s, Chapters: %02" PRIu32 ", Video streams: %02" PRIu8 ", Audio streams: %02" PRIu8 ", Subtitles: %02" PRIu8 ", Filesize: %05.0lf MBs\n", bluray_title.number, bluray_title.playlist, bluray_title.length, bluray_title.chapters, bluray_title.video_streams, bluray_title.audio_streams, bluray_title.pg_streams, bluray_title.size_mbs);
+	// libmpv doesn't support Blu-ray angle selection (as of latest stable, 0.29.1)
+	printf("Title: %03" PRIu32 ", Playlist: %04" PRIu32 ", Length: %s, Chapters: %02" PRIu32 ", Video streams: %02" PRIu8 ", Audio streams: %02" PRIu8 ", Subtitles: %02" PRIu8 ", Angles: %02" PRIu32 ", Filesize: %05.0lf MBs\n", bluray_title.number, bluray_title.playlist, bluray_title.length, bluray_title.chapters, bluray_title.video_streams, bluray_title.audio_streams, bluray_title.pg_streams, bluray_title.angles, bluray_title.size_mbs);
 
 	// Finished with libbluray
 	bd_close(bd);
