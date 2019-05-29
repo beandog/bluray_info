f27a48d41b33b48c0e1ec4124c2ee8799cc96615
Steve Dibb <steve.dibb@gmail.com>
2019-05-29 02:06:38 -0600

    Fix extra space

--- bluray_time.c
+++ bluray_time.c
@@ -25,7 +25,7 @@ void bluray_duration_length(char *str, const uint64_t duration) {
 	uint64_t d_secs = msecs % 60;
 	uint64_t d_msecs = (duration / 900) % 100;
 
-	sprintf(str, "%02" PRIu64 ":%02" PRIu64  ":%02" PRIu64 ".%02" PRIu64, d_hours, d_mins, d_secs, d_msecs);
+	sprintf(str, "%02" PRIu64 ":%02" PRIu64 ":%02" PRIu64 ".%02" PRIu64, d_hours, d_mins, d_secs, d_msecs);
 
 }
 
